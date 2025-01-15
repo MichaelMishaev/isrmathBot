0b2887ef1fd92598e159947ba6277a80fae9e2da
Michael <34490316+MichaelMishaev@users.noreply.github.com>
2025-01-15 08:49:38 +0200

    update json handle and promp

--- mathProj/obj/Debug/net8.0/mathProj.AssemblyInfo.cs
+++ mathProj/obj/Debug/net8.0/mathProj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mathProj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c95602b62b715868b8ba44951824a93342372b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e62fd01620de682fafa4f07518c422a5eba63c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("mathProj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mathProj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
